da86a84bf3901b39f58dbeeea5fc7bcb1c7958b3
unknown <Uuga-Chaka>
2018-05-16 05:22:58 -0500

    final touch, naisu

--- public/js/checkout.js
+++ public/js/checkout.js
@@ -8,7 +8,12 @@ fetch('http://localhost:1236/productosPorIds?ids='+arreglo)
     console.log(res.legth);
 
     var lista = document.querySelector(".container");
+    var precio = document.querySelector("#total");
+    var buy = document.querySelector(".buyed");
+    var total = 0;
+    console.log(precio);
     res.forEach(function(elem){
+        total += parseInt(elem.price);
         lista.innerHTML += '<div class="producto" data-id="'
         + elem._id+'"> <a href="/producto/'
         + elem._id+'"><img src="img/'+elem.object+'-'
@@ -17,5 +22,8 @@ fetch('http://localhost:1236/productosPorIds?ids='+arreglo)
         +elem.price+'</div><p class="desc">This '+elem.object+'s price is $'+elem.price+' its color is '
         +elem.color+' is very usefull, and cannot nevere be destroyed, and its size is '+elem.inches+
         ' inches</p></a><button class="agregar">Remove</button></div>';
+      
+        precio.innerHTML = '$ '+total;
+        buy.innerHTML += '<li><span>'+elem.object+'-'+elem.color+': </span>   $'+elem.price+'</li>'
     });
 });
